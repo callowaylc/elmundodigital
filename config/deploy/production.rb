14f28823a1f881456977e62ad35b1a44e24fd577
Ubuntu <ubuntu@ip-10-71-152-78.ec2.internal>
2014-01-26 01:11:18 +0000

    added web1 to production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,4 +8,4 @@ set :stage,  'production'
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
 #role :app, %w{deploy@example.com}
-role :web,  %w{ ubuntu@web0 } 
+role :web,  %w{ ubuntu@web0 ubuntu@web1 } 
